b20e196e013bce5b235273b263b0ede3e40deb67
kaike <97672711+mmxxkk@users.noreply.github.com>
2022-07-08 17:48:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style lang="less">
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
